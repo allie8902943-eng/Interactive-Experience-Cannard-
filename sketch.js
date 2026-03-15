1417c06a2c4ed033324cd27a0b2165f89156fd5e
allie8902943-eng <allie8902943@gmail.com>
2026-03-14 17:18:37 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+let myImage1;
+
+function preLoad () {
+   myImage1 = loadImage('background.png');
+}
+
+function setup() {
+  createCanvas(800, 600);
+  Image(myImage1, 0, 0);
+}
+
+function draw() {
+
+}
